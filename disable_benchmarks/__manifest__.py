45548292d5b462e917d56c9b99618e23c2c287d3
tripodeservices <74548992+tripodeservices@users.noreply.github.com>
2024-12-04 10:02:57 +0100

    update

--- disable_benchmarks/__manifest__.py
+++ disable_benchmarks/__manifest__.py
@@ -2,6 +2,13 @@
     'name': 'Disable Benchmarks',
     'version': '16.0',
     'summary': 'Désactive la route /web/benchmarks, même sans le module web',
+    'description': """
+        <p><strong>Fonctionnalités principales :</strong></p>
+        <ul>
+            <li>Désactive la route /web/benchmarks</li>
+        </ul>
+        <p>Pour plus d'informations, visitez notre <a href="https://votresite.com">site web</a>.</p>
+    """,
     'author': 'Tripode-Services',
     'depends': [],  # Pas de dépendance requise
     'installable': True,
